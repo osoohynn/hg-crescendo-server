aaccbd5a8925150fe5edbaf4fe3fd31a2e14bb4c
su080915 <tbr335@naver.com>
2024-08-25 17:51:41 +0900

    create server

---
+++ crescendo-server/src/main/java/com/example/crescendoserver/CrescendoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.crescendoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescendoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrescendoServerApplication.class, args);
+    }
+
+}
---
+++ crescendo-server/src/test/java/com/example/crescendoserver/CrescendoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crescendoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescendoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
