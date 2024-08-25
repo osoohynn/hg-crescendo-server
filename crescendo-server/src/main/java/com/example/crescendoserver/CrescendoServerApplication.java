e451237a2dd939323ee795fa849f146c56f53216
su080915 <tbr335@naver.com>
2024-08-25 23:02:21 +0900

    edit7

--- crescendo-server/src/main/java/com/example/crescendoserver/CrescendoServerApplication.java
+++ crescendo-server/src/main/java/com/example/crescendoserver/CrescendoServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrescendoServerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CrescendoServerApplication.class, args);
     }
